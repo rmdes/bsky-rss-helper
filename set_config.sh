485f7987d6b918f6248f2da69c203be5ad44b9a3
Ricardo <rick@armada.digital>
2024-02-12 19:29:41 +0100

    Update set_config.sh
    update to latest v2

--- set_config.sh
+++ set_config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-# this scripts is at the root of the parent folder where your newsbots are kept
-# and it will check each sub folder and set the config.json inside each /data/ directory
-# for each of the bots. 
+# This script is at the root of the parent folder where your newsbots are kept.
+# It will check each subfolder and set the config.json inside each /data/ directory
+# for each of the bots.
 #
-# set config.json with default or missing values
-# carefull this overwrite existing config.json values
-# warning: the Languages value is set/defined by the manner your folder name is named
-# so for example, yourbot-en and yourbot-fr will set config.json to use EN and FR languages string. 
-# if your folder are not following this naming convention, your language string will defautl to "En"
-# feel free to change the master config below if some of the fields are not needed for your usecase.
+# Set config.json with default or missing values
+# Careful, this overwrites existing config.json values.
 
 # Default master config.json as a string
 master_config=$(cat <<EOM
 {
   "string": "$title",
   "publishEmbed": true,
+  "embedType": "card",
   "languages": ["en"],
+  "ogUserAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0",
   "truncate": true,
   "runInterval": 60,
   "dateField": "",
   "imageField": "",
-  "forceDescriptionEmbed": true,
+  "imageAlt": "$description",
+  "forceDescriptionEmbed": false,
+  "removeDuplicate": true,
   "descriptionClearHTML": true,
-  "ogUserAgent": ""
+  "titleClearHTML": true
 }
 EOM
 )
@@ -30,7 +30,7 @@ EOM
 # Loop through all subfolders
 for dir in */; do
   # Extract parent folder name
-  parent_folder=$(basename $dir)
+  parent_folder=$(basename "$dir")
 
   # Extract language from parent folder name
   lang="${parent_folder##*-}"
@@ -39,10 +39,10 @@ for dir in */; do
   config_path="${dir}data/config.json"
 
   # Write the master_config to config.json, overwriting if it exists
-  echo "$master_config" > $config_path
+  echo "$master_config" > "$config_path"
 
   # Modify "languages" based on the parent folder name
   if [[ "$lang" == "en" || "$lang" == "fr" ]]; then
-    jq --arg lang "$lang" '.languages = [$lang]' $config_path > temp.json && mv temp.json $config_path
+    jq --arg lang "$lang" '.languages = [$lang]' "$config_path" > temp.json && mv temp.json "$config_path"
   fi
 done
