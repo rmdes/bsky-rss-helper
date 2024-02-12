ca672a0869b0865a2a12398eea913342bf9c7baa
Ricardo <rick@armada.digital>
2024-02-12 19:30:22 +0100

    Update up_config.sh
    update variables to latest v2

--- up_config.sh
+++ up_config.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-# this scripts is at the root of the parent folder where your newsbots are kept
-# and it will check each sub folder and set the config.json inside each /data/ directory
-# for each of the bots. 
-
-# it Update config.json without overwriting existing fields
-
-
-#!/bin/bash
+# This script is at the root of the parent folder where your newsbots are kept.
+# It checks each subfolder and sets the config.json inside each /data/ directory
+# for each of the bots without overwriting existing fields.
 
 # Loop through all subfolders
 for dir in */; do
   # Extract parent folder name
-  parent_folder=$(basename $dir)
+  parent_folder=$(basename "$dir")
 
   # Extract language from parent folder name
   lang="${parent_folder##*-}"
@@ -20,8 +15,23 @@ for dir in */; do
   config_path="${dir}data/config.json"
   
   # Check if config.json exists
-  if [[ -f $config_path ]]; then
-    # Update only missing fields and languages
-    jq --arg lang "$lang" '.string = (.string // "$title ") | .publishEmbed = (.publishEmbed // true) | .languages = (.languages // [$lang]) | .truncate = (.truncate // true) | .runInterval = (.runInterval // 60) | .dateField = (.dateField // "") | .imageField = (.imageField // "") | .forceDescriptionEmbed = (.forceDescriptionEmbed // true) | .descriptionClearHTML = (.descriptionClearHTML // true) | .ogUserAgent = (.ogUserAgent // "")' $config_path > temp.json && mv temp.json $config_path
+  if [[ -f "$config_path" ]]; then
+    # Update only missing fields and languages, including new fields
+    jq --arg lang "$lang" \
+       --arg ogUserAgent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0" \
+       '.string = (.string // "$title") | 
+        .publishEmbed = (.publishEmbed // true) | 
+        .embedType = (.embedType // "card") | 
+        .languages = (.languages // [$lang]) | 
+        .ogUserAgent = (.ogUserAgent // $ogUserAgent) | 
+        .truncate = (.truncate // true) | 
+        .runInterval = (.runInterval // 60) | 
+        .dateField = (.dateField // "") | 
+        .imageField = (.imageField // "") | 
+        .imageAlt = (.imageAlt // "$description") | 
+        .forceDescriptionEmbed = (.forceDescriptionEmbed // false) | 
+        .removeDuplicate = (.removeDuplicate // true) | 
+        .descriptionClearHTML = (.descriptionClearHTML // true) | 
+        .titleClearHTML = (.titleClearHTML // true)' "$config_path" > temp.json && mv temp.json "$config_path"
   fi
 done
